6fd4eaf3739ffb80f92e8fae67d0dc2d4e75105b
pinkycat <puknotdead@yandex.ru>
2019-12-13 17:24:47 +0400

    install bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>title</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<div class="jumbotron">
+    <h1 class="display-3"><?php echo'Hello, world!' ?> </h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+</div>
+
+<div class="card mb-3">
+
+        <img style="width: 100%; display: block;" src="images/test.jpg"/>
+    <div class="card-body">
+        <p class="text-muted">Здесь будет выводиться фотография</p>
+
+    </div>
+
+
+    <div class="card-footer text-muted">
+        2 days ago
+        <span style="padding: 0 10px;" class="text-muted">|</span>
+            <span class="badge badge-pill badge-primary">Primary</span>
+            <span class="badge badge-pill badge-light">Light</span>
+            <span class="badge badge-pill badge-dark">Dark</span>
+        <span style="padding: 0 10px;" class="text-muted">|</span>
+        <div style="display: inline-block">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+        </div>
+
+    </div>
+</div>
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div>
+
+
 <?php
 
 
@@ -5,4 +89,17 @@ echo 'GIT ZAEBAL';
 
 echo 'git zarabotal';
 
-echo 'using phpstorm';
+echo 'using phpstorm'; ?>
+
+</body>
+</html>
+
+
+<?php
+
+
+echo 'GIT ZAEBAL';
+
+echo 'git zarabotal';
+
+echo 'using phpstorm'; ?>
